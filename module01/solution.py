ab4793485a967be8d9fcd8dcff1689905b2c712d
Gennaro Coppola <gennaro.coppola@outlook.com>
2021-01-01 21:43:43 +0200

    Module1 solution

---
+++ module01/solution.py
@@ -0,0 +1,11 @@
+#Very basic exercises
+
+def f_c(X):
+    ''' Returns a constant value of 4 '''
+    return 4
+
+def f_x(x, a, b):
+    return a * x + b
+
+def sum(x):
+    return f_x(x,1,1) + f_x(x,2,2) + f_x(x,3,3) 
