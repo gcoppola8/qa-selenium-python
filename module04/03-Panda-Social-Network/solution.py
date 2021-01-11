b7ccfc6b49f168d2da2bee0f30a4f443eca886c5
Gennaro Coppola <coppola612@gmail.com>
2021-01-11 21:03:23 +0200

    Module 4, exercise 3 solution

--- module04/03-Panda-Social-Network/solution.py
+++ module04/03-Panda-Social-Network/solution.py
@@ -1,22 +1,3 @@
-'''
-ivo = Panda("Ivo", "ivo@pandamail.com", "male")
-
-ivo.name() == "Ivo" # True
-ivo.email() == "ivo@pandamail.com"  # True
-ivo.gender() == "male" # True
-ivo.isMale() == True # True
-ivo.isFemale() == False # True
-```
-
-The `Panda` class also should be possible to:
-
-    * Be turned into a string
-    * Be hashed and used as a key in a dictionary (`__eq__` and `__hash__`)
-    * Make sure that the email is a valid email!
-
-    Two `Panda` instances are equal if they have matching `name`, `email` and `gender` attributes.
-'''
-
 class Panda:
     def __init__(self, name, email, gender):
         self._name = name
@@ -51,18 +32,10 @@ class Panda:
     def __hash__(self):
         return hash((self._name, self._email, self._gender))
 
-'''
-`add_panda(panda)` - this method adds a panda to the social network. The panda has 0 friends for now.
-If the panda is already in the network, raise a `PandaAlreadyThere` error.
-`has_panda(panda)` - returns `True` or `False` if the panda is in the network or not.
-`make_friends(panda1, panda2)` - makes the two pandas friends. Raise `PandasAlreadyFriends` if they are already friends.
-The friendship is two-ways - `panda1` is a friend with `panda2` and `panda2` is a friend with `panda1`.
-If `panda1` or `panda2` are not members of the network, add them!
-`are_friends(panda1, panda2)` - returns `True` if the pandas are friends. Otherwise, `False`
-`friends_of(panda)` - returns a list of `Panda` with the friends of the given panda.
-Returns `False` if the panda is not a member of the network.
-'''
 class PandaAlreadyThere(Exception):
+    None
+
+class PandaAlreadyFriends(Exception):
     None
 
 class PandaSocialNetwork:
@@ -78,6 +51,12 @@ class PandaSocialNetwork:
         return panda in self._network
 
     def make_friends(self, panda1, panda2):
+        if panda1 not in self._network:
+            self.add_panda(panda1)
+        if panda2 not in self._network:
+            self.add_panda(panda2)
+        if panda1 in self._network[panda2]:
+            raise PandaAlreadyFriends
         self._network[panda1].append(panda2)
         self._network[panda2].append(panda1)
 
@@ -85,4 +64,6 @@ class PandaSocialNetwork:
         return panda2 in self._network[panda1]
 
     def friends_of(self, panda):
+        if panda not in self._network:
+            return False
         return self._network[panda]
